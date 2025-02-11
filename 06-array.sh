59ead24839d60855b5cf59f7c3c09a04365e5763
sai <saichandrikasasi@gmail.com>
2025-02-11 12:36:19 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -3,9 +3,9 @@
 FRUITS=("APPLE" "OARNGE" "KIWI")
 
 echo "first fruit is : ${FRUITS[0]}"
-echo "first fruit is : ${FRUITS[1]}"
-echo "first fruit is : ${FRUITS[2]}"
-
+echo "second fruit is : ${FRUITS[1]}"
+echo "third fruit is : ${FRUITS[2]}"
+echo "list of all fruits : ${FRUITS[@]}"
 
 
 
