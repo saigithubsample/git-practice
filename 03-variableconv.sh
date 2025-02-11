417fb41b2b5b34727c45660165b183fa251a6136
sai <saichandrikasasi@gmail.com>
2025-02-11 12:05:53 +0530

    variableconv

---
+++ 03-variableconv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=nani
+PERSON2=geethu
+
+echo "$PERSON1: Hi $PERSON2.How are you?"
+echo "$PERSON2: Hi $PERSON1 Im good who do you do?"
+echo "$PERSON1: im doing great $PERSON2. wht about you?"
+echo "$PERSON2: im learning devops $PERSON1"
