6603c8b1cc74ba0b607f7c013084ef8c4728871b
sai <saichandrikasasi@gmail.com>
2025-02-11 12:24:37 +0530

    variable with invi

---
+++ 05-invuser.sh
@@ -0,0 +1,11 @@
+#!@/bin/bash
+echo "please enter your username"
+
+read -s USERNAME
+
+echo "username u entered is : $USERNAME"
+
+echo "pleae enter the password"
+
+read -s PASSWORD
+echo "the password u entered is: $PASSWORD"
