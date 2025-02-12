6feebe5c344930c19e434c5289fde74f1ac3fa4b
sai <saichandrikasasi@gmail.com>
2025-02-12 10:59:34 +0530

    date

--- 13-instalation.sh
+++ 13-instalation.sh
@@ -8,12 +8,19 @@ echo "please try to access with root previlages"
 exit 1
 fi
 
-dnf list installed git 
+dnf list installed gitt 
 
 if [ $? -ne 0 ]
  then
   echo "git is not installed"
-     dnf install git -y
+     dnf install gitt -y
+
+     if [ $? -ne 0 ]
+      then
+       echo "git installation is not successfull try again"
+       else
+        echo "git installation is success"
+        fi
      else
      echo "git is already installe dnothing to do"
 fi
